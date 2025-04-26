54832af723d643daee6af099162d079e618d7cc1
Rasi Max <rasimax55@gmail.com>
2025-04-26 23:28:22 +0530

    Add initial HTML structure for Piggy365 application

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ import bodyParser from "body-parser";
 import connectDataBase from "./Config/DB.js";
 import router from "./Routes/routers.js";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors("*"));
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 connectDataBase();
 app.use("/api", router);
+
+// app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+// S
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
